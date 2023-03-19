6b20a871e1953487d3ae5b190d44957bd9b449eb
dasileker <dasileker@gmail.com>
2023-03-19 16:03:54 +0000

    add useState to add more questions

--- src/components/QuizTrainning/questionTrainning/QuestionTrainning.js
+++ src/components/QuizTrainning/questionTrainning/QuestionTrainning.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'reactstrap';
 import ImageUploader from '../../Criteria/ImageUploader/ImageUploader';
 import ImagLoader from '../ImageLoader/ImageLoader';
@@ -15,68 +15,65 @@ const QuestionTrainning = () => {
     setQuestion2Image(file);
   };
 
+  const [questions, setQuestions] = useState([
+    {
+      id: 1,
+      text: '',
+      trueImage: '',
+      falseImage: ''
+    }
+  ]);
+
+  const addQuestion = () => {
+    setQuestions(prevQuestions => [
+      ...prevQuestions,
+      {
+        id: prevQuestions.length + 1,
+        text: '',
+        trueImage: '',
+        falseImage: ''
+      }
+    ]);
+  };
+
   return (
     <div>
-      <Row>
-        <Col xs="12" md="6">
-          <Row>
-            <Col xs="10">
-              <p>Question 1</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6">
-              <input type="checkbox" id="true1" />
-              <label htmlFor="true1">True</label>
-            </Col>
-            <Col xs="2">
-              <ImagLoader />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6">
-              <input type="checkbox" id="false1" />
-              <label htmlFor="false1">False</label>
-            </Col>
-            <Col xs="2">
-              <ImagLoader />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs="12" md="6">
-          <ImageUploader onFileUpload={handleQuestion1ImageUpload} question="1" />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs="12" md="6">
-          <Row>
-            <Col xs="10">
-              <p>Question 2</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6">
-              <input type="checkbox" id="true2" />
-              <label htmlFor="true2">True</label>
-            </Col>
-            <Col xs="2">
-              <ImagLoader />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6">
-              <input type="checkbox" id="false2" />
-              <label htmlFor="false2">False</label>
-            </Col>
-            <Col xs="2">
-              <ImagLoader />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs="12" md="6">
-          <ImageUploader onFileUpload={handleQuestion2ImageUpload} question="2" />
-        </Col>
-      </Row>
+      {questions.map((question) => (
+        <Row key={question.id}>
+          <Col xs="12" md="6">
+            <Row>
+              <Col xs="10">
+                <p>Question {question.id}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6">
+                <input type="checkbox" id={`true${question.id}`} />
+                <label htmlFor={`true${question.id}`}>True</label>
+              </Col>
+              <Col xs="2">
+                <ImagLoader />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6">
+                <input type="checkbox" id={`false${question.id}`} />
+                <label htmlFor={`false${question.id}`}>False</label>
+              </Col>
+              <Col xs="2">
+                <ImagLoader />
+              </Col>
+            </Row>
+          </Col>
+          <Col xs="12" md="6">
+            <ImageUploader onFileUpload={handleQuestion1ImageUpload} question={question.id} />
+          </Col>
+        </Row>
+      ))}
+      <button onClick={addQuestion} style={{ background: 'lightblue', color: 'white', width: '100%', padding: '10px', border: 'none', borderRadius: '5px', marginTop: '20px' }}>
+        Add Question
+        <span style={{ marginLeft: '5px' }}>+</span>
+      </button>
     </div>
   );
 };
